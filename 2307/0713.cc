6fd22bb3425a0cf3ae5cee60f33bc74779b8ae45
bianyishupian <bianyichips@outlook.com>
2023-07-13 15:04:07 +0800

    new

--- 2307/0713.cc
+++ 2307/0713.cc
@@ -29,6 +29,7 @@ namespace leetCode51
     vector<vector<string>> result;
     /*
         leetCode 51. N 皇后
+
         n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
         给你一个整数 n ，返回所有不同的 n 皇后问题 的解决方案。
         每一种解法包含一个不同的 n 皇后问题 的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
@@ -93,6 +94,7 @@ namespace leetCode704
 {
     /*
         leetCode 704. 二分查找
+
         给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，
         写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
 
@@ -119,6 +121,76 @@ namespace leetCode704
 
 } // namespace leetCode704
 
+namespace leetCode27
+{
+    /*
+        leetCode 27. 移除元素
+
+        给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+        不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+        元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+        输入：nums = [3,2,2,3], val = 3
+        输出：2, nums = [2,2]
+        解释：函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。你不需要考虑数组中超出新长度后面的元素。
+              例如，函数返回的新长度为 2 ，而 nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案。
+    */
+    int removeElement(vector<int> &nums, int val)
+    {
+        int res = 0, n = nums.size();
+        for (int slow = 0, fast = 0; fast < n; fast++)
+        {
+            if (nums[fast] == val)
+            {
+                res++;
+                continue;
+            }
+            else
+            {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return n - res;
+    }
+} // namespace leetCode27
+
+namespace leetCode977
+{
+    /*
+        leetCode 977. 有序数组的平方
+
+        给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+
+        输入：nums = [-4,-1,0,3,10]
+        输出：[0,1,9,16,100]
+        解释：平方后，数组变为 [16,1,0,9,100]
+              排序后，数组变为 [0,1,9,16,100]
+    */
+    vector<int> sortedSquares(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> res(n, 0);
+        int left = 0, right = n - 1, pos = n - 1;
+        while (left <= right)
+        {
+            int l = nums[left] * nums[left];
+            int r = nums[right] * nums[right];
+            if (l < r)
+            {
+                res[pos--] = r;
+                right--;
+            }
+            else
+            {
+                res[pos--] = l;
+                left++;
+            }
+        }
+        return res;
+    }
+} // namespace leetCode977
+
 int main()
 {
     // leetCode 51. N 皇后
@@ -142,6 +214,30 @@ int main()
         cout << "leetCode 704. 二分查找: " << endl;
         vector<int> nums{-1, 0, 3, 5, 9, 12};
         cout << leetCode704::search(nums, 9) << endl; // 4
+        cout << endl;
+    }
+
+    // leetCode 27. 移除元素
+    {
+        cout << "leetCode 27. 移除元素: " << endl;
+        vector<int> nums{3, 2, 2, 3};
+        int n = leetCode27::removeElement(nums, 3);
+        for (int i = 0; i < n; i++)
+        {
+            cout << nums[i] << " ";
+        }
+        cout << endl;
+    }
+
+    // leetCode 977. 有序数组的平方
+    {
+        cout << "leetCode 977. 有序数组的平方: " << endl;
+        vector<int> nums{-4, -1, 0, 3, 10};
+        vector<int> res = leetCode977::sortedSquares(nums);
+        for (int i : res)
+        {
+            cout << i << " ";
+        }
         cout << endl;
     }
 
