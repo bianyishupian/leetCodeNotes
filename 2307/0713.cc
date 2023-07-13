acb9f19e3285c472ff16e0cacbe6a29f8eb6d990
bianyishupian <bianyichips@outlook.com>
2023-07-13 12:09:30 +0800

    new

--- 2307/0713.cc
+++ 2307/0713.cc
@@ -24,6 +24,9 @@ struct ListNode
 
 namespace leetCode51
 {
+    bool isValid(int n, int row, int col, vector<string> &chessboard);
+    void backtracking(vector<string> &chessboard, int n, int row);
+    vector<vector<string>> result;
     /*
         leetCode 51. N 皇后
         n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
@@ -36,15 +39,111 @@ namespace leetCode51
     */
     vector<vector<string>> solveNQueens(int n)
     {
+        result.clear();
+        vector<string> chessboard(n, string(n, '.'));
+        backtracking(chessboard, n, 0);
+        return result;
+    }
+    void backtracking(vector<string> &chessboard, int n, int row)
+    {
+        if (row == n)
+        {
+            result.push_back(chessboard);
+            return;
+        }
+        for (int col = 0; col < n; col++)
+        {
+            if (isValid(n, row, col, chessboard))
+            {
+                chessboard[row][col] = 'Q';
+                backtracking(chessboard, n, row + 1);
+                chessboard[row][col] = '.'; // 回溯
+            }
+        }
+    }
 
+    bool isValid(int n, int row, int col, vector<string> &chessboard)
+    {
+        // 检查列
+        for (int i = 0; i < row; i++)
+        {
+            if (chessboard[i][col] == 'Q')
+                return false;
+        }
+
+        // 检查 45度角是否有皇后
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+        {
+            if (chessboard[i][j] == 'Q')
+                return false;
+        }
+
+        // 检查 135度角是否有皇后
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
+        {
+            if (chessboard[i][j] == 'Q')
+                return false;
+        }
+        return true;
     }
 
 } // namespace leetCode51
 
+namespace leetCode704
+{
+    /*
+        leetCode 704. 二分查找
+        给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，
+        写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+
+        输入: nums = [-1,0,3,5,9,12], target = 9
+        输出: 4
+        解释: 9 出现在 nums 中并且下标为 4
+    */
+    int search(vector<int> &nums, int target)
+    {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return -1;
+    }
+
+} // namespace leetCode704
+
 int main()
 {
-    
+    // leetCode 51. N 皇后
+    {
+        cout << "leetCode 51. N 皇后: " << endl;
+        vector<vector<string>> res;
+        res = leetCode51::solveNQueens(4);
+        for (vector<string> a : res)
+        {
+            for (string s : a)
+            {
+                cout << s << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
 
+    // leetCode 704. 二分查找
+    {
+        cout << "leetCode 704. 二分查找: " << endl;
+        vector<int> nums{-1, 0, 3, 5, 9, 12};
+        cout << leetCode704::search(nums, 9) << endl; // 4
+        cout << endl;
+    }
 
     return 0;
 }
